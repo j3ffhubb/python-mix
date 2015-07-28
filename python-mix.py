3f3652a691232ecf0e849bf744f70b61d46bfb1e
MusiKernel Team <musikernel@nospam.org>
2015-07-27 22:17:06 -0400

    Print the peak amplitude after creating the file
    (this will plug into a forthcoming --normalize CLI option)

--- python-mix.py
+++ python-mix.py
@@ -4,6 +4,7 @@
     License: http://www.gnu.org/licenses/gpl-3.0.en.html
 """
 
+import math
 import os
 import sys
 try:
@@ -19,6 +20,12 @@ except ImportError:
 if len(sys.argv) < 4:
     print("Usage: {0} output_file file1 file2 ...".format(__file__))
     exit(1)
+
+def lin_to_db(a_value):
+    if a_value >= 0.001:
+        return math.log(float(a_value), 10.0) * 20.0
+    else:
+        return -120.0
 
 OUTPUT_FILE = sys.argv[1]
 FILES = sys.argv[2:]
@@ -39,6 +46,7 @@ if len(SAMPLE_RATES) != 1:
     exit(1)
 
 SR = tuple(SAMPLE_RATES)[0]
+MAX = 0.0
 
 with wavefile.WaveWriter(OUTPUT_FILE, channels=2, samplerate=SR) as writer:
     while GENERATORS:
@@ -55,6 +63,11 @@ with wavefile.WaveWriter(OUTPUT_FILE, channels=2, samplerate=SR) as writer:
                     BUFFER[i,:data.shape[1]] += data[0]
             else:
                 BUFFER[:,:data.shape[1]] += data
+        buffer_max = numpy.amax(numpy.abs(BUFFER))
+        if buffer_max > MAX:
+            MAX = buffer_max
         writer.write(BUFFER)
 
 print("Created {0}".format(OUTPUT_FILE))
+print("Maximum amplitude {0}dB".format(round(lin_to_db(MAX), 1)))
+
